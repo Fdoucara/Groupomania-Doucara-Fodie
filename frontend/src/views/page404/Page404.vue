e0dadfb46a5b424bb367ae46d2deb099f7e29c20
Doucara Fodié <fodiedoucara8@gmail.com>
2022-07-25 16:47:18 +0200

    99% on clean le code

---
+++ frontend/src/views/page404/Page404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page404',
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped src="./page404.scss">
+</style>

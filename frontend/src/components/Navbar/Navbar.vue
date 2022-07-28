d7ea67c1724ace26bce2a133ecbd464ecabb8627
Doucara Fodié <fodiedoucara8@gmail.com>
2022-07-28 08:04:36 +0200

    mise en place du responsive

--- frontend/src/components/Navbar/Navbar.vue
+++ frontend/src/components/Navbar/Navbar.vue
@@ -1,22 +1,29 @@
 <template>
-  <div>
+  <div class="contain">
+    <div class="overlay"></div>
+    <div class="burger" @click="burgerMenu">
+      <span></span>
+    </div>
     <nav class="navbar">
       <router-link to="/accueil"> <img src="../../assets/logomodif1.png" alt="Logo Groupomania" class="navbar_logo">
       </router-link>
       <div class="navbar_item">
-        <i class="fas fa-home"></i>
+        <router-link to="/accueil"> <i class="fas fa-home"></i> </router-link>
         <router-link to="/accueil" class="navbar_link">
           <p class="navbar_link_p"> Accueil </p>
         </router-link>
       </div>
       <div class="navbar_item">
-        <i class="fas fa-home"></i>
+        <router-link to=""> <i class="fas fa-user"></i> </router-link>
+      </div>
+      <div class="navbar_item">
+        <router-link to="/utilisateurs"> <i class="fas fa-users"></i> </router-link>
         <router-link to="/utilisateurs" class="navbar_link">
           <p class="navbar_link_p"> Utilisateurs </p>
         </router-link>
       </div>
       <div class="navbar_item">
-        <i class="fas fa-sign-out-alt"></i>
+        <i class="fas fa-sign-out-alt" @click="deconnexion"></i>
         <p class="navbar_link navbar_link_p" @click="deconnexion"> Deconnexion </p>
       </div>
 
@@ -36,9 +43,20 @@ export default {
         withCredentials: true,
         baseURL: 'http://localhost:3000/api/'
       }),
+      overlay: null,
+      burger: null,
+      navbar: null
     }
   },
   methods: {
+    burgerMenu() {
+      this.overlay = document.querySelector('.overlay');
+      this.burger = document.querySelector('.burger');
+      this.navbar = document.querySelector('.navbar');
+      this.overlay.classList.toggle('black')
+      this.burger.classList.toggle('active');
+      this.navbar.classList.toggle('show');
+    },
     deconnexion() {
       this.axiosInstance.get('user/logout')
         .then(() => {
